c1672e348288f24c42e71abd6281681498d8f0fd
root <miguel.mish@gmail.com>
2020-05-02 18:13:17 +0000

    solved a problem with functions import

--- etl.py
+++ etl.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
-from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql.functions import year, month, dayofmonth, dayofweek, hour, weekofyear, date_format, from_unixtime
 
 config = configparser.ConfigParser()
 config.read_file(open('dl.cfg'))
